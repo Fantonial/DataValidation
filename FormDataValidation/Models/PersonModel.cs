68424d6ab24e14af5dc172837a22c75e35fae860
Grace Fantonial <79076569+Fantonial@users.noreply.github.com>
2021-03-03 21:02:33 +0800

    exercise#2

---
+++ FormDataValidation/Models/PersonModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FormDataValidation.Models
+{
+    public class PersonModel
+    {
+        [Required]
+        [StringLength(20)]
+        public string Name { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public DateTime DOB { get; set; }
+
+        [Required]
+        [Range(18, 60)]
+        public int Age { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+
+        [Required]
+        public int Contact { get; set; }
+
+        [Required]
+        public string School { get; set; }
+
+    }
+}
